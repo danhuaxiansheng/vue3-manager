8d3187d83daa30b601776be9e9ba7573e0278190
danhuaxiansheng <646042371@qq.com>
2022-03-03 16:32:33 +0800

    提交界面代码

--- src/views/vab/audio/index.vue
+++ src/views/vab/audio/index.vue
@@ -29,10 +29,7 @@
       :offset="index > 0 ? 2 : 0"
     >
       <el-card :body-style="{ padding: '0px' }">
-        <img
-          src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png"
-          class="image"
-        />
+        <img src="" class="image" />
         <div style="padding: 14px">
           <span>好吃的汉堡</span>
           <div class="bottom clearfix">
